43fbbb5e7ecdff5c4d8481b810aea4e994d049b1
Eqidna <EqidnaQDNA@proton.me>
2023-07-23 08:13:50 -0400

    edit

--- pages/index.tsx
+++ pages/index.tsx
@@ -144,7 +144,11 @@ const Home: NextPage = () => {
                 $Pepe4U holds the key to the future, eagerly awaiting your
                 arrival. 
               </p>
-              
+              <iframe
+                          height="100%"
+                          width="100%"
+                          src="https://dexcheck.ai/app/polygon/chart/0x364cec393abbda65f887e497dfbdcc2a5a0fd66f?embed=true"
+                        ></iframe>
               <div style={{ position: "relative" }}>
                 <Image
                   src={RightImage}
